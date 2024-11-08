23540ca94ae637abaf92d0fb0190207b0b8c74af
Sean Clark <seaniboy2009@yahoo.co.uk>
2024-11-08 12:05:03 +0000

    chore: update to firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,33 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { User, getAuth, onAuthStateChanged } from "firebase/auth";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  query,
-  where,
-} from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, collection } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-type UserType = {
-  id: string;
-  email: string;
-  userId: string;
-  paid: boolean;
-  admin?: boolean;
-};
-
-type Note = {
-  id: string;
-  userId: string;
-  title: string;
-  category: string;
-  image: string;
-};
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -44,7 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 console.log("Firebase initialized");
-// const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
@@ -68,59 +42,59 @@ const dbNotes = collection(db, "notes");
 const dbLists = collection(db, "lists");
 const dbListItems = collection(db, "listItems");
 
-getDocs(dbUsers)
-  .then((snapshot) => {
-    let users: UserType[] = [];
+// getDocs(dbUsers)
+//   .then((snapshot) => {
+//     let users: UserType[] = [];
 
-    snapshot.docs.forEach((doc) => {
-      console.log("Get docs", doc.data());
-      const email = doc.data().email;
-      const userId = doc.data().userId;
-      const paid = doc.data().paid;
-      const admin = doc.data().admin;
+//     snapshot.docs.forEach((doc) => {
+//       console.log("Get docs", doc.data());
+//       const email = doc.data().email;
+//       const userId = doc.data().userId;
+//       const paid = doc.data().paid;
+//       const admin = doc.data().admin;
 
-      users.push({
-        ...doc.data(),
-        id: doc.id,
-        email,
-        userId,
-        paid,
-        admin,
-      });
-    });
+//       users.push({
+//         ...doc.data(),
+//         id: doc.id,
+//         email,
+//         userId,
+//         paid,
+//         admin,
+//       });
+//     });
 
-    console.log("users:", users);
-  })
-  .catch((error) => {
-    console.log("Error getting documents:", error);
-  });
+//     console.log("users:", users);
+//   })
+//   .catch((error) => {
+//     console.log("Error getting documents:", error);
+//   });
 
-getDocs(dbNotes)
-  .then((snapshot) => {
-    let notes: Note[] = [];
+// getDocs(dbNotes)
+//   .then((snapshot) => {
+//     let notes: Note[] = [];
 
-    snapshot.docs.forEach((doc) => {
-      console.log("Get docs", doc.data());
-      const userId = doc.data().userId;
-      const title = doc.data().email;
-      const category = doc.data().userId;
-      const image = doc.data().paid;
+//     snapshot.docs.forEach((doc) => {
+//       console.log("Get docs", doc.data());
+//       const userId = doc.data().userId;
+//       const title = doc.data().email;
+//       const category = doc.data().userId;
+//       const image = doc.data().paid;
 
-      notes.push({
-        ...doc.data(),
-        id: doc.id,
-        userId,
-        title,
-        category,
-        image,
-      });
-    });
+//       notes.push({
+//         ...doc.data(),
+//         id: doc.id,
+//         userId,
+//         title,
+//         category,
+//         image,
+//       });
+//     });
 
-    console.log("notes:", notes);
-  })
-  .catch((error) => {
-    console.log("Error getting documents:", error);
-  });
+//     console.log("notes:", notes);
+//   })
+//   .catch((error) => {
+//     console.log("Error getting documents:", error);
+//   });
 
 // Export Firebase services and functions
 export { auth, db, onAuthStateChanged, dbUsers, dbNotes, dbLists, dbListItems };
