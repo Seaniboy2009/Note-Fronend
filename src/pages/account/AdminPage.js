941717ea3a894bfa8deb5f99952a6e68abeb6411
Sean Clark <seaniboy2009@yahoo.co.uk>
2025-01-02 18:31:16 +0000

    feat: added calendar actions to add and remove

--- src/pages/account/AdminPage.js
+++ src/pages/account/AdminPage.js
@@ -2,9 +2,18 @@ import React, { useContext, useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import appStyle from "../../styles/App.module.css";
 import { useTheme } from "../../contexts/ThemeSelection";
-import { getDocs } from "firebase/firestore";
 import { useUser } from "../../contexts/UserContext";
-import { dbUsers } from "../../firebase";
+import { dbUsers, db } from "../../firebase";
+import {
+  doc,
+  addDoc,
+  getDocs,
+  collection,
+  query,
+  where,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
 import ThemedButton from "../../components/ThemedButton";
 
 const AdminPage = ({ props }) => {
@@ -12,6 +21,36 @@ const AdminPage = ({ props }) => {
   const { activeTheme, theme } = useTheme();
   const admin = userFirestore?.admin || false;
   const [users, setUsers] = useState([]);
+
+  const handleAdmin = (user) => {
+    console.log("handleAdmin", user);
+    getDocs(dbUsers).then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        if (doc.data().userId === user.userId) {
+          updateDoc(doc.ref, { admin: !doc.data().admin });
+        }
+      });
+    });
+    try {
+    } catch (error) {
+      console.error("Error updating admin: ", error);
+    }
+  };
+
+  const handleAdvancedUser = (user) => {
+    console.log("handleAdvancedUser", user);
+    getDocs(dbUsers).then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        if (doc.data().userId === user.userId) {
+          updateDoc(doc.ref, { advancedUser: !doc.data().advancedUser });
+        }
+      });
+    });
+    try {
+    } catch (error) {
+      console.error("Error updating advanced user: ", error);
+    }
+  };
 
   useEffect(() => {
     if (admin) {
@@ -66,10 +105,10 @@ const AdminPage = ({ props }) => {
               Email: {user.email} {user.admin ? "A" : null}{" "}
               {user.advancedUser ? "/ U" : null}
             </strong>{" "}
-            <ThemedButton onClick={() => console.log("Edit user")}>
+            <ThemedButton onClick={() => handleAdmin(user)}>
               {user.admin ? "Remove admin" : "Make admin"}
             </ThemedButton>
-            <ThemedButton onClick={() => console.log("Edit user")}>
+            <ThemedButton onClick={() => handleAdvancedUser(user)}>
               {user.advancedUser
                 ? "Remove advanced user"
                 : "Make advanced user"}
